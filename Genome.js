ec66461d8b89c9d5046bdffd4537f04717fbae7c
Hnatekmar <m.hnatma@gmail.com>
2018-10-23 11:06:36 +0200

    Migrated db to postgre

--- Genome.js
+++ Genome.js
@@ -3,21 +3,11 @@
  *  @param  {Object}  schema
  *  @return {Object}  model
  **/
-module.exports = function(schema){
-    const Genome = schema.define('genome', {
-        id: { type: schema.Integer },
-        genome: { type: schema.Json },
-        fitness: { type: schema.Float },
-        generation: { type: schema.Integer },
-        saveTime: { type: schema.Date, default: Date.now }
-    }, {
-        primaryKeys: ['id']
-    });
 
-    /**
-     *  Define any custom method
-     *  or setup validations here
-     **/
-
-    return Genome;
+module.exports = function (sequelize, Sequelize) {
+	return sequelize.define('genome', {
+		id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+		fitness: { type: Sequelize.FLOAT },
+		generation: { type: Sequelize.INTEGER }
+	});
 };
--- index.js
+++ index.js
@@ -4,114 +4,114 @@ const cluster = require('cluster');
 const os = require('os');
 
 if (cluster.isMaster) {
-    const caminte = require('caminte'),
-        Schema = caminte.Schema,
-        config = {
-            driver: 'sqlite3',
-            database: 'database.sqlite3'
-        };
+	const Sequelize = require('sequelize')
+	const options = {
+			dialect: 'postgres',
+			host: '192.168.1.52'
+		};
+	const sequelize = new Sequelize(
+		'postgres', 
+		'postgres', 
+		'example', options);
 
-    const schema = new Schema(config.driver, config);
 
-    const Genome = require('./Genome')(schema);
-    Genome.destroyAll();
+	const Genome = require('./Genome')(sequelize, Sequelize);
+	Genome.sync({force: true});
 
-    const NUMBER_OF_THREADS = os.cpus().length;
+	const NUMBER_OF_THREADS = os.cpus().length;
 
-    let neat = new NEAT.Neat(
-        37,
-        6, // LEFT, RIGHT, FORWARD, BACKWARDS, BREAK
-        null,
-        {
-            popsize: NUMBER_OF_THREADS * 128,
-            mutation: NEAT.methods.mutation.ALL,
-            mutationRate: 0.25
-        }
-    );
+	let neat = new NEAT.Neat(
+		37,
+		6, // LEFT, RIGHT, FORWARD, BACKWARDS, BREAK
+		null,
+		{
+			popsize: NUMBER_OF_THREADS * 128,
+			mutation: NEAT.methods.mutation.ALL,
+			mutationRate: 0.15
+		}
+	);
 
-    let bestScore = Number.MIN_VALUE;
-    function evolve () {
-        neat.sort();
-        console.log('Generation ' + neat.generation);
+	let bestScore = Number.MIN_VALUE;
+	function evolve () {
+		neat.sort();
+		console.log('Generation ' + neat.generation);
 
-        // From https://wagenaartje.github.io/neataptic/docs/neat/
-        let newPopulation = [];
+		// From https://wagenaartje.github.io/neataptic/docs/neat/
+		let newPopulation = [];
 
-        // Elitism
-        for (let i = 0; i < neat.elitism; i++) {
-            newPopulation.push(neat.population[i])
-        }
+		// Elitism
+		for (let i = 0; i < neat.elitism; i++) {
+			newPopulation.push(neat.population[i])
+		}
 
-        // Breed the next individuals
-        for (let i = 0; i < neat.popsize - neat.elitism; i++) {
-            newPopulation.push(neat.getOffspring())
-        }
+		// Breed the next individuals
+		for (let i = 0; i < neat.popsize - neat.elitism; i++) {
+			newPopulation.push(neat.getOffspring())
+		}
 
-        // Replace the old population with the new population
-        neat.population = newPopulation;
-        neat.mutate();
+		// Replace the old population with the new population
+		neat.population = newPopulation;
+		neat.mutate();
 
-        neat.generation++
-    }
+		neat.generation++
+	}
 
-    let children = [];
-    let count = 0;
-    for (let i = 0; i < NUMBER_OF_THREADS; i++) {
-        children.push(cluster.fork())
-    }
-    for (let i = 0; i < NUMBER_OF_THREADS; i++) {
-        children[i].on('message', (msg) => {
-            count += 1;
-            let json = neat.population[msg['index']].toJSON();
-            neat.population[msg['index']].score = msg['score']
-            let genome = new Genome({
-                fitness: msg['score'],
-                genome: json,
-                generation: neat.generation
-            });
-            genome.save()
-        })
-    }
-    _.chunk(neat.population, neat.population.length / children.length).forEach((chunk, index) => {
-        let jsonChunk = chunk.map((genome) => genome.toJSON());
-        children[index].send({
-            'json': jsonChunk,
-            'index': Math.floor(index * (neat.population.length / children.length))
-        })
-    });
-    setInterval(function () {
-        if (count === neat.population.length) {
-            evolve();
-            count = 0;
-            _.chunk(neat.population, neat.population.length / children.length).forEach((chunk, index) => {
-                let jsonChunk = chunk.map((genome) => genome.toJSON());
-                children[index].send({
-                    'json': jsonChunk,
-                    'index': Math.floor(index * (neat.population.length / children.length))
-                })
-            })
-        }
-    }, 50)
+	let children = [];
+	let count = 0;
+	for (let i = 0; i < NUMBER_OF_THREADS; i++) {
+		children.push(cluster.fork())
+	}
+	for (let i = 0; i < NUMBER_OF_THREADS; i++) {
+		children[i].on('message', (msg) => {
+			count += 1;
+			let json = neat.population[msg['index']].toJSON();
+			neat.population[msg['index']].score = msg['score']
+			Genome.create({
+				fitness: msg['score'],
+				generation: neat.generation
+			});
+		})
+	}
+	_.chunk(neat.population, neat.population.length / children.length).forEach((chunk, index) => {
+		let jsonChunk = chunk.map((genome) => genome.toJSON());
+		children[index].send({
+			'json': jsonChunk,
+			'index': Math.floor(index * (neat.population.length / children.length))
+		})
+	});
+	setInterval(function () {
+		if (count === neat.population.length) {
+			evolve();
+			count = 0;
+			_.chunk(neat.population, neat.population.length / children.length).forEach((chunk, index) => {
+				let jsonChunk = chunk.map((genome) => genome.toJSON());
+				children[index].send({
+					'json': jsonChunk,
+					'index': Math.floor(index * (neat.population.length / children.length))
+				})
+			})
+		}
+	}, 100)
 } else {
-    let { JSDOM } = require('jsdom');
-    let jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-    let { window } = jsdom;
-    global.window = window
-    global.document = window.document
-    global.navigator = {
-        userAgent: 'node.js',
-    };
+	let { JSDOM } = require('jsdom');
+	let jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+	let { window } = jsdom;
+	global.window = window
+	global.document = window.document
+	global.navigator = {
+		userAgent: 'node.js',
+	};
 
-    const Simulation = require('simulation').default
-    let simulation = new Simulation(60)
-    process.on('message', (info) => {
-        for (let i = 0; i < info['json'].length; i++) {
-            let genom = NEAT.Network.fromJSON(info['json'][i]);
-            let score = simulation.evalGenome(1 / 30.0, genom);
-            process.send({
-                'score': score,
-                'index': info['index'] + i
-            })
-        }
-    })
+	const Simulation = require('simulation').default
+	let simulation = new Simulation(60)
+	process.on('message', (info) => {
+		for (let i = 0; i < info['json'].length; i++) {
+			let genom = NEAT.Network.fromJSON(info['json'][i]);
+			let score = simulation.evalGenome(1 / 30.0, genom);
+			process.send({
+				score: score,
+				index: info['index'] + i
+			})
+		}
+	})
 }
