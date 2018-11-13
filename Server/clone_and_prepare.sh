9125c09028a2fdfaf3ffc1ce3b8d4832e8362232
Hnatekmar <m.hnatma@gmail.com>
2018-11-13 21:09:17 +0100

    Better build

---
+++ Server/clone_and_prepare.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/Hnatekmar/Thesis-node.git
+
+cd Thesis-node/Server
+yarn install
+node server.js
--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,18 @@
 const cluster = require('cluster');
+
 if (!cluster.isMaster) {
-	const express = require('express');
-	let { JSDOM } = require('jsdom');
-	const bodyParser = require('body-parser');
-	const NEAT = require('neataptic');
-	const compression = require('compression');
-	let jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-	let { window } = jsdom;
+    let { JSDOM } = require('jsdom');
+    const bodyParser = require('body-parser');
+    const NEAT = require('neataptic');
+    const compression = require('compression');
+    let jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+    let { window } = jsdom;
     global.window = window
     global.document = window.document
+
     global.navigator = {
         userAgent: 'node.js',
     };
-    const app = express();
-    const port = 3000;
-    app.listen(port);
-    app.use(bodyParser.json({limit:'100mb'}));
-    app.use(compression());
 
     const Simulation = require('simulation').default;
 
@@ -25,13 +21,44 @@ if (!cluster.isMaster) {
         genome = NEAT.Network.fromJSON(genome);
         return simulation.evalGenome(1.0 / 60.0, genome);
     }
-
-    app.post('/evaluate', function (req, res) {
-        res.json(req.body.map(evalGenome));
-    })
 } else {
+    const express = require('express');
     const cpuCount = require('os').cpus().length;
+
     for (let i = 0; i < cpuCount; i++) {
         cluster.fork();
     }
+
+    const app = express();
+    const port = 3000;
+    app.listen(port);
+    app.use(bodyParser.json({limit:'100mb'}));
+    app.use(compression());
+    app.post('/evaluate', function (req, res) {
+	let id = Date.now();
+	_.chunk(req.body, threads.length).map((chunk, index) => {
+		threads[i].send({
+					genomes: chunk,
+					id: id,
+					index: index
+                                }, null, callback.bind({threadCount: threads.length}));
+	});
+	let results = [];
+	for (let i = 0; i < threads.length; i++) {
+		results[i] = null;
+	}
+	Process.on('message', (response) => {
+		if(response.id === id) {
+			results[index] = response;	
+		}
+		if(results.every((x) => x !== null)) {
+			res.json(results.reduce((acc, value) => {
+				return acc.concat(value);
+			}, []));
+			for (let i = 0; i < threads.length; i++) {
+				results[i] = null;
+			}
+		}
+	});
+    })
 }
